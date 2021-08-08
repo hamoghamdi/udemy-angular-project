d8faa64c0a8fa6bed9d4b77d2eea77f40a713e28
hamoghamdi <haneen.aljabrah@gmail.com>
2021-08-08 17:38:27 +0300

    structure: add recipe-item component, components init content structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RecipesComponent } from './Recipes/components/recipes/recipes.component';
+import { RecipeListComponent } from './Recipes/components/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './Recipes/components/recipe-item/recipe-item.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { ShoppingListComponent } from './ShoppingList/components/shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './ShoppingList/components/shopping-edit/shopping-edit.component';
+import { RecipeDetailComponent } from './Recipes/components/recipe-detail/recipe-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent,
+    RecipeDetailComponent
   ],
   imports: [
     BrowserModule
